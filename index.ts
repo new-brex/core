a42893c50d3cb878004bd730c2143979627c6824
Kian <kianhooshmand@berkeley.edu>
2022-08-11 13:56:55 -0400

    update index to add main() method

--- index.ts
+++ index.ts
@@ -3,24 +3,36 @@ import dotenv from "dotenv";
 
 import * as financials from "./src/financials/card";
 
-dotenv.config();
+async function main() {
+  dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT;
+  const app: Express = express();
+  const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server is cool");
-});
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server is cool");
+  });
 
-app.get("/stripeTest", async (req: Request, res: Response) => {
-  const stripeAccount = await financials.registerDAOWithStripe();
-  console.log("Created and verified Stripe connected account:", stripeAccount.id);
-  const financialAccount = await financials.createBankAccount(stripeAccount.id);
-  console.log("Created and funded financial account:", financialAccount.id);
-  const card = await financials.createCard(stripeAccount.id, financialAccount.id);
-  res.send(`${JSON.stringify(card)}`);
-})
+  app.get("/stripeTest", async (req: Request, res: Response) => {
+    const stripeAccount = await financials.registerDAOWithStripe();
+    console.log(
+      "Created and verified Stripe connected account:",
+      stripeAccount.id
+    );
+    const financialAccount = await financials.createBankAccount(
+      stripeAccount.id
+    );
+    console.log("Created and funded financial account:", financialAccount.id);
+    const card = await financials.createCard(
+      stripeAccount.id,
+      financialAccount.id
+    );
+    res.send(`${JSON.stringify(card)}`);
+  });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+  app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  });
+}
+
+main();
