288f5ad97a6764f33f4cb9eb7adeff18820be314
Kian <kianhooshmand@berkeley.edu>
2022-08-11 16:47:53 -0400

    add db helpers and fix seeding

--- index.ts
+++ index.ts
@@ -2,25 +2,60 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
 import * as financials from "./src/financials/card";
+import {
+  ConnectionContext,
+  establishConnection,
+} from "./src/blockchain/connection";
+import { Database } from "./src/database/database";
+import { readFileSync } from "fs";
 
-dotenv.config();
+async function main() {
+  dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT;
+  const app: Express = express();
+  const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server is cool");
-});
+  const context: ConnectionContext = establishConnection("devnet");
+  const db: Database = new Database();
 
-app.get("/stripeTest", async (req: Request, res: Response) => {
-  const stripeAccount = await financials.registerDAOWithStripe();
-  console.log("Created and verified Stripe connected account:", stripeAccount.id);
-  const financialAccount = await financials.createBankAccount(stripeAccount.id);
-  console.log("Created and funded financial account:", financialAccount.id);
-  const card = await financials.createCard(stripeAccount.id, financialAccount.id);
-  res.send(`${JSON.stringify(card)}`);
-})
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server is cool");
+  });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+  app.get("/stripeTest", async (req: Request, res: Response) => {
+    const stripeAccount = await financials.registerDAOWithStripe();
+    console.log(
+      "Created and verified Stripe connected account:",
+      stripeAccount.id
+    );
+    const financialAccount = await financials.createBankAccount(
+      stripeAccount.id
+    );
+    console.log("Created and funded financial account:", financialAccount.id);
+    const card = await financials.createCard(
+      stripeAccount.id,
+      financialAccount.id
+    );
+    res.send(`${JSON.stringify(card)}`);
+  });
+
+  app.get("/seedDB", async (req: Request, res: Response) => {
+    console.log("Seeding DB");
+    const seedQuery = readFileSync("./src/database/seeding.sql", {
+      encoding: "utf-8",
+    });
+    await db.query(seedQuery);
+    console.log("Database seeded");
+  });
+
+  app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  });
+
+  console.log("Seeding DB");
+  const seedQuery = readFileSync("./src/database/seeding.sql", {
+    encoding: "utf-8",
+  });
+}
+
+main();
