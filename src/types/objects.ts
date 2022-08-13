2b8320788bfcc7e68755b49d7d15c0ea44d981ba
Kian <kianhooshmand@berkeley.edu>
2022-08-13 18:57:48 -0400

    add proposal transaction type

--- src/types/objects.ts
+++ src/types/objects.ts
@@ -11,21 +11,25 @@ export interface Proposal {}
  */
 export interface Transaction {}
 
+export interface ProposalTransaction extends Transaction {
+  name: string;
+}
+
 /**
  * Abstraction for any blockchain-based entity (ex. a Realm)
  */
 export interface DAO {
-    symbol?: string | undefined,
-    displayName?: string | undefined,
-    programId?: string | undefined,
-    realmId?: string | undefined,
-    bannerImage?: string | undefined,
-    ogImage?: string | undefined,
-    sharedWalletId?: string | undefined,
-    sortRank?: number | undefined,
-    keywords?: string | undefined,
-    twitter?: string | undefined,
-    website?: string | undefined
+  symbol?: string | undefined;
+  displayName?: string | undefined;
+  programId?: string | undefined;
+  realmId?: string | undefined;
+  bannerImage?: string | undefined;
+  ogImage?: string | undefined;
+  sharedWalletId?: string | undefined;
+  sortRank?: number | undefined;
+  keywords?: string | undefined;
+  twitter?: string | undefined;
+  website?: string | undefined;
 }
 
 export interface MultiSigDetails {}
